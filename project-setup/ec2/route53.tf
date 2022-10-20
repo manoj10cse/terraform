19cbe5b84019cee7ae123fb81a99a0564d43ebcb
patchigolla santhosh <santhosh.ramana96@gmail.com>
2022-10-20 19:09:49 +0530

    ser

--- project-setup/ec2/route53.tf
+++ project-setup/ec2/route53.tf
@@ -1,6 +1,6 @@
-esource "aws_route53_record" "component" {
+resource "aws_route53_record" "component" {
   zone_id = "Z04602961I29SHWLCRCU3"
-  name    = "${var.COMPONENT}-dev.santhosh.roboshop.internal"
+  name    = "${var.COMPONENT}-dev.santhoshroboshop.internal"
   type    = "A"
   ttl     = 10
   records = [aws_spot_instance_request.spot_worker.private_ip]
